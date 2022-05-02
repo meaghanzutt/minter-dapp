44e47721002d4d8e8cf61f7dffcbda79b5b8c547
meaghanzutt <meaghanzutt@gmail.com>
2022-05-01 17:54:34 -0700

    update

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,24 +1,24 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xC08941C961a806A88C16658A7C5066Dd59b11999"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to Ms Iwaleilani'z NFT Shop. Have a mint!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
-const h1_presale_mint = "Pre-Sale Minting Open!!";
+const welcome_p = "The musuWAHINE project is a decentralized, Hawaiian owned project that aims to demonstrate how to develope and launch your own NFT Collection, onboard friends and family to Web 3 and create awareness for the Hawaiian culture and history";
+const h1_presale_coming_soon = "First Drop Coming Soon!!";
+const h1_presale_mint = "Hiki Mua Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
-const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
+const h2_presale_coming_soon = "Hiki Mua Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
 const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
-const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
-const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
-const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
-const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
-const button_presale_coming_soon = "Get on the Whitelist";
-const button_presale_mint_whitelisted = "Mint Your Special NFT";
-const button_presale_mint_not_whitelisted = "Get on the Whitelist";
-const button_presale_already_minted = "Join The Community";
-const button_public_mint = "Mint Your NFT";
+const p_presale_mint_not_whitelisted = "You are not on the Hiki Mua list for the pre-sale.. 😢";
+const p_presale_mint_whitelisted = "You're on the Hiki Mua list for the pre-sale! 🎉";
+const p_presale_mint_already_minted = "You've already claimed your Hiki Mua mint. Thank you! 🎉";
+const p_public_mint = "No Hiki Mua needed. Public minting is now open! 🎉";
+const button_presale_coming_soon = "Get on the Hiki Mua list";
+const button_presale_mint_whitelisted = "Mint Your Yummy Musubi";
+const button_presale_mint_not_whitelisted = "Get on the Hiki Mua list";
+const button_presale_already_minted = "Join The Ohana";
+const button_public_mint = "Mint Your musuWAHINE";
 const mint_failed = "Minting failed. 😢 Please try again.";
 
 const abi = [
